9d4b4cd9fabbc5e3399d94719ec70c7cc4348cce
shirah <shirah.davis@gmail.com>
2017-05-03 14:52:19 -0400

    initial commit

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Date Published")] [DisplayFormat(DataFormatString = "{0:mm/dd/yy}")]
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategotyID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Bookshelf/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
